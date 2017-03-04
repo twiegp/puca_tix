ffdc2633d21c8fa65274c9ca6281cf865ebb99c0
twiegp <twiegp@gmail.com>
2017-03-04 11:04:24 -0800

    Updated to post-bounty world and 3/17

--- tix_analysis.R
+++ tix_analysis.R
@@ -1,33 +1,34 @@
 library(tidyverse)
 
-thresholds <- c(10,100,1000)
+thresholds <- c(10,100)
 blacklist.names <- c("500% BONUS ON TIX")
+blacklist.dates <- c("2017-02-10","2017-02-11")
 
-df.tix <- read.csv("tix.csv") %>% filter(!(Name %in% blacklist.names))
-df.tix$Date <- as.Date(parse_datetime(as.character(df.tix$Time)))
-df.tix$Full_Price <- df.tix$Price + df.tix$Bounty
+df.tix <- read.csv("output.csv", stringsAsFactors = FALSE)
+df.tix$Date <- as.Date(parse_datetime(as.character(df.tix$time)))
+df.tix$Full_Price <- ifelse(as.logical(df.tix$promoted), as.integer(df.tix$price), 100 + as.integer(df.tix$bounty))
+df.tix$balance <- as.integer(gsub(",","",df.tix$balance))
+
+df.tix <-  df.tix %>% filter(!(user %in% blacklist.names | as.character(Date) %in% blacklist.dates) & promoted == 'True')
 
 df.thresholds <- tibble()
 
+# No need to get fancy here with the combinatorics of what a balance can pay for. Not worth it.
 for (i in 1:length(unique(df.tix$Date))) {
   date <- unique(df.tix$Date)[i]
-  print(date)
-  df.date <- filter(df.tix, Date == date) %>% 
-    group_by(UID) %>%
-    summarise(Q_Max = min(max(Q_Max),sum(Q)), Full_Price = max(Full_Price)) %>%
+  df.date <- filter(df.tix, Date == date & balance >= Full_Price) %>% 
     arrange(desc(Full_Price))
-  df.date$Cumulative_Max <- cumsum(df.date$Q_Max)
   
   for (val in thresholds) {
-    idx <- Position(function(x) x >= val, df.date$Cumulative_Max)
-    P <- as.double(df.date[idx,"Full_Price"])
+    P <- as.double(df.date[val,"Full_Price"])
     df.thresholds <- rbind(df.thresholds,
                            tibble(Date=date, Price=P, Threshold=as.factor(val)))
   }
 }
 
 price_plot <- ggplot(df.thresholds, aes(x=Date, y=Price, color=Threshold)) +
-  geom_line() +
+  geom_point() +
+  geom_smooth() +
   labs(y="Price",x="Date",color="Marginal Offer for X Tix") +
   ggtitle("MTGO Ticket Prices on PucaTrade") +
   theme(axis.title.x = element_text(size=12),
@@ -37,6 +38,8 @@ price_plot <- ggplot(df.thresholds, aes(x=Date, y=Price, color=Threshold)) +
         legend.title = element_text(size=20),
         legend.text = element_text(size=10),
         strip.text = element_text(size=14)) +
-  ylim(100,400)
+  xlim(as.Date("2017-02-01"),as.Date("2017-03-03")) +
+  ylim(200,300) +
+  geom_vline(xintercept=as.numeric(as.Date("2017-02-02")))
 
 ggsave("tix.png", price_plot, height = 8, width = 12)
